dd24a2aa31de11b36fe17c8cf973841938154dcc
S4NDSTROM <krille.sandstrom@gmail.com>
2019-10-13 10:43:03 +0200

    minor env changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 console.log(
   'initing ----------------------------------------------------------------',
 );
+console.debug(`The app is running in ${process.env.NODE_ENV} mode`);
 
 const app = require('./server/app');
 
